0acdf20e457316409c44e9b0d32286d8dff6e63a
avijit biswas sagar <saggarr.work@gmail.com>
2024-04-16 08:14:01 +0600

    ssl commerz step by step follow kore code a file gula copy paste kora hoyeche. .env te base url change kora hoyeche r ssl sandbox a ipn a base url boshano hoyeche

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\CustomerDashboardController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SslCommerzPaymentController;
 use App\Http\Controllers\SubCategoryController;
 use App\Http\Controllers\UnitController;
 use Illuminate\Support\Facades\Route;
@@ -41,6 +42,21 @@ Route::get('/delete-shopping-cart/{id}', [CartController::class, 'delete'])->nam
 Route::post('/update-shopping-cart/{id}', [CartController::class, 'update'])->name('cart.update');
 
 Route::get('/cart', [CartController::class, 'cart'])->name('cart');
+
+
+// SSLCOMMERZ Start
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
 
 
 Route::get('/check-out', [CheckoutController::class, 'index'])->name('checkout');
